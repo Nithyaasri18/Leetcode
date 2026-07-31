0536aba256f9403a1d79dc2395b04a6c96412802
Nithyaasri18 <nithyaa1812@gmail.com>
2026-07-31 22:22:19 +0530

    [643] [Time Beats: 42.32%] [Memory Beats: 45.11%] - LeetPush

---
+++ 643-Maximum-Average-Subarray-I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double windowsum=0;
+        for(int i=0;i<k;i++)
+        {
+            windowsum+=nums[i];
+        }
+        double maxsum=windowsum;
+        for(int i=k;i<nums.length;i++)
+        {
+            windowsum=windowsum-nums[i-k]+nums[i];
+            maxsum=Math.max(maxsum,windowsum);
+        }
+        return maxsum/k;
+        
+    }
+}
