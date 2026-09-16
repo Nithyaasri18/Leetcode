5a5c34eb6232fd38736c0a45b79987d561a8a5b2
Nithyaasri18 <nithyaa1812@gmail.com>
2026-09-16 23:24:04 +0530

    [3898] [Time Beats: 100.00%] [Memory Beats: 23.61%] - LeetPush

---
+++ 3898-Find-the-Degree-of-Each-Vertex.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+
+        int n=matrix.length;
+        int[]ans=new int[matrix.length];
+
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=0;j<n;j++)
+            {
+                sum+=matrix[i][j];
+            }
+            ans[i]=sum;
+        }
+        return ans;
+        
+    }
+}
