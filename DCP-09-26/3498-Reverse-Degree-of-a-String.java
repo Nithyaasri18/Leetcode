49b668f5dcea1cc5e1a2ad2b0ada731894a38cbb
Nithyaasri18 <nithyaa1812@gmail.com>
2026-09-20 22:14:38 +0530

    [3498] [Time Beats: 100.00%] [Memory Beats: 91.21%] - LeetPush

---
+++ DCP-09-26/3498-Reverse-Degree-of-a-String.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseDegree(String s) {
+
+        int sum=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            int r=26-(ch-'a');
+            int pos=i+1;
+            sum+=r*pos;
+        }
+        return sum;
+        
+    }
+}
